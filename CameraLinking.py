f2b68636711d1a21b86ddb387b573c40d68aa422
auram <auramalexander@gmail.com>
2023-11-08 02:24:39 -0600

    Added UI panel in the viewport sidebar

--- CameraLinking.py
+++ CameraLinking.py
@@ -2,7 +2,7 @@ bl_info = {
     "name": "Camera Linking",
     "description": "Control visibility of collections based on active camera.",
     "author": "Dylan Alexander",
-    "version": (0, 2),
+    "version": (0, 3),
     "blender": (3, 3, 0),  # Blender version compatibility
     "category": "Camera",
     "auto": True
@@ -64,14 +64,62 @@ def add_camera_custom_properties(dummy):
                 if prop_name not in [camera.name for camera in cameras]:
                     del collection[prop_name]
                     
+### UI ###
+
+class CustomCategory(bpy.types.PropertyGroup):
+    name = bpy.props.StringProperty(name="Camera Linking", default="Camera Linking")
+
+bpy.utils.register_class(CustomCategory)
+bpy.types.Scene.my_custom_category = bpy.props.PointerProperty(type=CustomCategory)
+
+
+# Define a Panel for the Properties Editor
+class CustomPropertiesPanel(bpy.types.Panel):
+    bl_label = "Camera Linking"
+    bl_idname = "PT_CustomPropertiesPanel"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = 'Camera Linking'
+
+    def draw(self, context):
+        layout = self.layout
+
+        # Get the selected object
+        obj = context.object
+        if obj is not None:
+            collection = obj.users_collection[0]
+            collection_name = collection.name
+
+            # Check if the selected collection has custom properties
+            if collection_name in bpy.data.collections:
+                custom_props = bpy.data.collections[collection_name].items()
+
+                # Create a column for custom properties
+                column = layout.column()
+                column.label(text=f"{collection_name}:")
+
+                for prop_name, prop_value in custom_props:
+                    # Create a row for each custom property
+                    row = layout.row()
+
+                    # Add a checkbox
+                    row.prop(bpy.data.collections[collection_name], f'["{prop_name}"]', text='', toggle=True)
+
+                    # Add the custom property name and value
+                    row.label(text=prop_name)
+                    row.prop(bpy.data.collections[collection_name], prop_name)
+
+                    
 ### REGISTER & RUN ###
 
 # Registration
 def register():
+    bpy.utils.register_class(CustomPropertiesPanel)
     bpy.app.handlers.depsgraph_update_post.append(add_camera_custom_properties)
     bpy.app.handlers.frame_change_post.append(toggle_visibility)
 
 def unregister():
+    bpy.utils.unregister_class(CustomPropertiesPanel)
     if toggle_visibility in bpy.app.handlers.frame_change_post:
         bpy.app.handlers.frame_change_post.remove(toggle_visibility)
     if add_camera_custom_properties in bpy.app.handlers.depsgraph_update_post:
